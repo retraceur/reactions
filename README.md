ff32b8fdf0c3f4237305fa41e410b1f7bf728694
imath <imathi.eu@outlook.fr>
2025-04-19 07:23:29 +0200

    Inform this is a WIP plugin as a feature

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# reactions
-Une extension de Retraceur pour gérer les réactions à un article (commentaires, appréciations...)
+# Réactions
+
+> [!CAUTION]
+> This Retraceur Plugin is a Work In Progress: do not install on a production site.
+
+Réactions is currently mainly a "Placeholder" plugin storing next [Retraceur](https://github.com/retraceur/coeur) Post Reaction features (eg: comments, webmentions, likes etc...). Feel free to contribute to it.
---
+++ reactions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Réactions
+ * Plugin URI: https://github.com/retraceur/reaction/
+ * Description: Retraceur's Reaction feature as a plugin.
+ * Version: 1.0.0-alpha
+ * Requires PHP: 5.6
+ * Requires Retraceur: 1.0.0
+ * Up to Retraceur:    1.0.0
+ * License: GNU/GPL 2
+ * Author: imath
+ * Author URI: https://imathi.eu/
+ * Text Domain: retraceur-reaction
+ * Domain Path: /languages/
+ * Network: True
+ * GitHub Plugin URI: https://github.com/retraceur/reaction/
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
