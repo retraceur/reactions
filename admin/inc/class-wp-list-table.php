8d8334f266ba23bd20205dfaf4a41e88b83c39b0
imath <math.viet@gmail.com>
2026-09-26 11:32:20 +0200

    Administration: Support compact button shorthand See WP 7.1 commit: 0c6c00f41d2fa72b35b95e46004c526086c4301f

--- admin/inc/class-wp-list-table.php
+++ admin/inc/class-wp-list-table.php
@@ -411,7 +411,7 @@ class WP_List_Table {
 <p class="search-box">
 	<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo $text; ?>:</label>
 	<input type="search" id="<?php echo esc_attr( $input_id ); ?>" name="s" value="<?php _admin_search_query(); ?>" />
-		<?php submit_button( $text, '', '', false, array( 'id' => 'search-submit' ) ); ?>
+		<?php submit_button( $text, 'compact', '', false, array( 'id' => 'search-submit' ) ); ?>
 </p>
 		<?php
 	}
@@ -627,7 +627,7 @@ class WP_List_Table {
 
 		echo "</select>\n";
 
-		submit_button( __( 'Apply' ), 'action', 'bulk_action', false, array( 'id' => "doaction$two" ) );
+		submit_button( __( 'Apply' ), 'action compact', 'bulk_action', false, array( 'id' => "doaction$two" ) );
 		echo "\n";
 	}
 
